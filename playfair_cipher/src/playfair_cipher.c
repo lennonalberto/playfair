01162633a2ea73c44a5b9511510f7a97d83336bf
Leonardo Wistuba <leonardowistuba@gmail.com>
2013-06-07 21:43:35 -0300

    Initial Commit

--- playfair_cipher/src/playfair_cipher.c
+++ playfair_cipher/src/playfair_cipher.c
@@ -1,7 +1,32 @@
+/*
+  ============================================================================
+  APPROACH
+  ============================================================================
+1. Generate a random key, called the 'parent',  decipher the ciphertext
+    using this key. Rate the fitness of the deciphered text, store the result.
+2. Change the key slightly (swap two characters in the key at random),
+    measure the fitness of the deciphered text using the new key.
+3. If the fitness is higher with the modified key, store it as our current
+    key and discard the parent.
+4. Go back to 2, unless no improvement in fitness occurred in the last 1000
+    iterations.
+  ============================================================================
+  SOURCE
+  ============================================================================
+  http://practicalcryptography.com/cryptanalysis/stochastic-searching/cryptanalysis-playfair/
+  ============================================================================
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts("BANANA"); /* prints  */
-	return EXIT_SUCCESS;
+void simulatedAnnealing();
+
+int main(int argc, char* argv[]) {
+	simulatedAnnealing();
+
+}
+
+void simulatedAnnealing() {
+
 }
